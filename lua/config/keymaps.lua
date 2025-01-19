be0e5b43880565db0cbf4b938a37b9b9e07c6a22
Jl115 <joelbern006@gmail.com>
2025-01-19 19:49:25 +0100

    Updated plugins colorshceme and keybindings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,18 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+return {
+  {
+    "custom.keymaps",
+    keys = {
+      -- Save buffer
+      { "<leader>bs", "<cmd>w<cr>", desc = "Save Buffer" },
+
+      -- Exit insert mode with `ii`
+      { "ii", "<Esc>", mode = "i", desc = "Exit Insert Mode with ii" },
+
+      -- Remap Shift+U to redo (Ctrl+R)
+      { "U", "<C-r>", mode = "n", desc = "Redo" },
+    },
+  },
+}
