11a61666a4187c843a2268459cddb8cab300ddaf
Jl115 <joelbern006@gmail.com>
2025-01-31 05:04:26 +0100

    final config initial setup

---
+++ lua/plugins/rainbow.lua
@@ -0,0 +1,16 @@
+if true then
+  return {}
+end
+-- return {
+--   {
+--     "lukas-reineke/indent-blankline.nvim",
+--     main = "ibl",
+--     opts = function(_, opts)
+--       -- Other blankline configuration here
+--       return require("indent-rainbowline").make_opts(opts)
+--     end,
+--     dependencies = {
+--       "TheGLander/indent-rainbowline.nvim",
+--     },
+--   },
+-- }
