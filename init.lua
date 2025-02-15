231b1567b51dc3aedc16aface1f87f1d2c34e8f0
Jl115 <joelbern006@gmail.com>
2025-02-15 19:43:21 +0100

    feat: added mini.files

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 --
-vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
 
 require("config.lazy")
+vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,53 +1,23 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        -- pyright will be automatically installed with mason and loaded with lspconfig
-        pyright = {},
-      },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        -- lua_ls will be automatically installed with mason and loaded with lspconfig
-        lua_ls = {},
-      },
-    },
-  },
-
-  -- add tsserver and setup with typescript.nvim instead of lspconfig
-  {
-    "neovim/nvim-lspconfig",
     dependencies = {
       "jose-elias-alvarez/typescript.nvim",
-      init = function()
-        require("lazyvim.util").lsp.on_attach(function(_, buffer)
-          -- stylua: ignore
-          vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
-          vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
-        end)
-      end,
     },
-    ---@class PluginLspOpts
     opts = {
-      ---@type lspconfig.options
       servers = {
-        -- tsserver will be automatically installed with mason and loaded with lspconfig
+        pyright = {},
+        lua_ls = {},
         tsserver = {},
+        dartls = {},
+        rust_analyzer = {},
       },
-      -- you can do any additional lsp server setup here
-      -- return true if you don't want this server to be setup with lspconfig
-      ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
       setup = {
-        -- example to setup with typescript.nvim
         tsserver = function(_, opts)
+          -- 1. Setup typescript.nvim FIRST
+          require("typescript").setup({ server = opts })
+
+          -- 2. THEN, modify the opts (especially root_dir)
           local util = require("lspconfig.util")
           opts.root_dir = function(fname)
             local js_root = util.search_ancestors(fname, function(dir)
@@ -60,28 +30,25 @@ return {
             end
             return util.root_pattern("package.json", "tsconfig.json", ".git")(fname)
           end
-          require("typescript").setup({ server = opts })
-          return true
+
+          -- 3. No need to return true; nvim-lspconfig will handle it.
         end,
-        -- Specify * to use this function as a fallback for any server
-        -- ["*"] = function(server, opts) end,
       },
-    },
-  },
+      on_attach = function(client, buffer) -- Add client argument
+        -- ... (Your keybindings and logging from the previous example)
 
-  -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
-  -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
-  { import = "lazyvim.plugins.extras.lang.typescript" },
-  { import = "lazyvim.plugins.extras.lang.json" },
-
-  -- Add LSP support for Dart and Rust
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        dartls = {},           -- Dart language server
-        rust_analyzer = {},    -- Rust language server
-      },
+        if client.name == "tsserver" then
+          -- stylua: ignore
+          vim.api.nvim_buf_set_keymap(buffer, "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
+          vim.api.nvim_buf_set_keymap(
+            buffer,
+            "n",
+            "<leader>cR",
+            "TypescriptRenameFile",
+            { buffer = buffer, desc = "Rename File" }
+          )
+        end
+      end,
     },
   },
 }
